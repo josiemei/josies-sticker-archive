91375ccba693cecbe1b1e3fb353d3158090a1664
josiemei <josiethemeister@gmail.com>
2025-06-18 10:23:27 -0700

    Fixed index.html

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
 document.write('\
 \
-<div align="center" class="header"><a href="home.html"><img src="images/header.svg" align="center" style="width:50%" title="Home"></a></div>\
+<div align="center" class="header"><a href="index.html"><img src="images/header.svg" align="center" style="width:50%" title="Home"></a></div>\
 <br>\
 <br>\
 <table align="center" class="nav" style="width:48%; text-align:center;">\
   <tr class="nav-row">\
-    <td class="nav-element"><a href="home.html" class="link">Home</a></td>\
+    <td class="nav-element"><a href="index.html" class="link">Home</a></td>\
     <td class="nav-element"><a href="stickers.html" class="link">Stickers</a></td>\
     <td class="nav-element"><a href="info-faqs.html" class="link">Info & FAQs</a></td>\
     <td class="nav-element"><a href="contact-me.html" class="link">Contact Me</a></td>\
