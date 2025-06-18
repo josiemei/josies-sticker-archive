09a37949b9c3127974ba55dcd23fdbde06fda187
josiemei <josiethemeister@gmail.com>
2025-06-18 11:05:57 -0700

    Fixed Buy Me a Coffee URL

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ document.write('\
 <div align="center" class="header"><a href="index.html"><img src="images/header.svg" align="center" style="width:50%" title="Home"></a></div>\
 <br>\
 <br>\
-<table align="center" class="nav" style="width:48%; text-align:center;">\
+<table align="center" class="nav" style="width:60%; text-align:center;">\
   <tr class="nav-row">\
     <td class="nav-element"><a href="index.html" class="link">Home</a></td>\
     <td class="nav-element"><a href="stickers.html" class="link">Stickers</a></td>\
@@ -11,6 +11,6 @@ document.write('\
     <td class="nav-element"><a href="contact-me.html" class="link">Contact Me</a></td>\
   </tr>\
 </table>\
-<div class="socials"><a href="https://buymeacoffee.com/" target="_blank"><img src="images/buymeacoffee.png" width="32px" title="Buy Me a Coffee"></a></div>\
+<div class="socials"><a href="https://coff.ee/josiemeister" target="_blank"><img src="images/buymeacoffee.png" width="32px" title="Buy Me a Coffee"></a></div>\
 \
 ');
